ec50811979e74250cdedf62e6e399749f42f0884
Pankaj Joshi <pankaj.joshi.acs@gmail.com>
2015-07-27 13:33:53 +0900

    Update config.php

--- admin/config.php
+++ admin/config.php
@@ -1,31 +1,31 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://sunrosejapan.com/grocery/admin/');
-define('HTTP_CATALOG', 'http://sunrosejapan.com/grocery/');
-define('HTTP_IMAGE', 'http://sunrosejapan.com/grocery/image/');
+define('HTTP_SERVER', 'YOUR_ADMIN_DIR');
+define('HTTP_CATALOG', 'YOUR_CATALOG_DIR');
+define('HTTP_IMAGE', 'YOUR_CATALOG_IMAGE_DIR');
 
 // HTTPS
 define('HTTPS_SERVER', '');
 define('HTTPS_IMAGE', '');
 
 // DIR
-define('DIR_APPLICATION', '/home/gkdlxyvb/public_html/grocery/admin/');
-define('DIR_SYSTEM', '/home/gkdlxyvb/public_html/grocery/system/');
-define('DIR_DATABASE', '/home/gkdlxyvb/public_html/grocery/system/database/');
-define('DIR_LANGUAGE', '/home/gkdlxyvb/public_html/grocery/admin/language/');
-define('DIR_TEMPLATE', '/home/gkdlxyvb/public_html/grocery/admin/view/template/');
-define('DIR_CONFIG', '/home/gkdlxyvb/public_html/grocery/system/config/');
-define('DIR_IMAGE', '/home/gkdlxyvb/public_html/grocery/image/');
-define('DIR_CACHE', '/home/gkdlxyvb/public_html/grocery/system/cache/');
-define('DIR_DOWNLOAD', '/home/gkdlxyvb/public_html/grocery/download/');
-define('DIR_LOGS', '/home/gkdlxyvb/public_html/grocery/system/logs/');
-define('DIR_CATALOG', '/home/gkdlxyvb/public_html/grocery/catalog/');
+define('DIR_APPLICATION', '/home/DIR/public_html/grocery/admin/');
+define('DIR_SYSTEM', '/home/DIR/public_html/grocery/system/');
+define('DIR_DATABASE', '/home/DIR/public_html/grocery/system/database/');
+define('DIR_LANGUAGE', '/home/DIR/public_html/grocery/admin/language/');
+define('DIR_TEMPLATE', '/home/DIR/public_html/grocery/admin/view/template/');
+define('DIR_CONFIG', '/home/DIR/public_html/grocery/system/config/');
+define('DIR_IMAGE', '/home/DIR/public_html/grocery/image/');
+define('DIR_CACHE', '/home/DIR/public_html/grocery/system/cache/');
+define('DIR_DOWNLOAD', '/home/DIR/public_html/grocery/download/');
+define('DIR_LOGS', '/home/DIR/public_html/grocery/system/logs/');
+define('DIR_CATALOG', '/home/DIR/public_html/grocery/catalog/');
 
 // DB
 define('DB_DRIVER', 'mysql');
-define('DB_HOSTNAME', 'localhost');
-define('DB_USERNAME', 'gkdlxyvb');
-define('DB_PASSWORD', 'WlCnvLpJ');
-define('DB_DATABASE', 'gkdlxyvb_grocery');
+define('DB_HOSTNAME', 'YOUR_DB_HOST');
+define('DB_USERNAME', 'YOUR_DB_USER_NAME');
+define('DB_PASSWORD', 'YOUR_DB_PASSWORD');
+define('DB_DATABASE', 'YOUR_DB_NAME');
 define('DB_PREFIX', '');
 ?>
